f29f3dd3e4342f0eff1890c6cbd06e965821e6f0
Jonathan Buhler <benedict.buhler@gmail.com>
2016-05-21 16:50:56 -0400

    Block class
    Barebones block abstract class with basic functions

---
+++ Block.java
@@ -0,0 +1,18 @@
+public abstract class Block {
+
+  private int resiliance;
+  private String type;
+  private String texture;
+
+  public int getResiliance () {
+    return resiliance;
+  }
+
+  public String getType () {
+    return type;
+  }
+
+  public String getTexture () {
+    return texture;
+  }
+}
